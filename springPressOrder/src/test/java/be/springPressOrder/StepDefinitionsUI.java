45fbe09212f27d375a797cd8d147fd80cf09b4e0
oBoii <fabian.denoodt@student.odisee.be>
2019-11-22 17:09:18 +0100

    Aplied chrome driver

--- springPressOrder/src/test/java/be/springPressOrder/StepDefinitionsUI.java
+++ springPressOrder/src/test/java/be/springPressOrder/StepDefinitionsUI.java
@@ -1,40 +1,29 @@
 package be.springPressOrder;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxBinary;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
-
-import java.io.File;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class StepDefinitionsUI {
 
 
-    File pathToBinary = new File("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
-    FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
-    FirefoxProfile firefoxProfile = new FirefoxProfile();
-
-    WebDriver driver; // = new FirefoxDriver(ffBinary,firefoxProfile);
+    WebDriver driver;
 
 
     @Given("^particulier is ingelogd$")
     public void particulierIsIngelogd() {
-        // driver = new FirefoxDriver();
-        System.setProperty("webdriver.gecko.driver", "D:\\onedrive\\OneDrive - ODISEE\\Fabian\\Jaar 2\\Semester 2\\Software\\w6\\geckodriver.exe");
-        driver = new FirefoxDriver(ffBinary, firefoxProfile);
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Fabian\\Downloads\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.navigate().to("http://localhost:8080/login");
 
-//        driver = new FirefoxDriver();
-        driver.navigate().to("http://localhost:8080/");
-//        driver.findElement(By.id("username")).sendKeys("admin");
-//        driver.findElement(By.id("password")).sendKeys("password");
-//
-//        driver.findElement(By.id("btnSubmit")).click();
+        driver.findElement(By.id("username")).sendKeys("admin");
+        driver.findElement(By.id("password")).sendKeys("password");
+
+        driver.findElement(By.id("btnSubmit")).click();
     }
 
     @And("^de particulier begeeft zich op de pagina om een nieuwe pressorder aan te maken$")
