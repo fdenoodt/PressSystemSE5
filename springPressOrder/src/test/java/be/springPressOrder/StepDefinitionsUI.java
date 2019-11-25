a0f856e75b59ca687952dbcc1e688518ca5757cf
oBoii <fabian.denoodt@student.odisee.be>
2019-11-25 13:17:52 +0100

    fix scenario's in UI

--- springPressOrder/src/test/java/be/springPressOrder/StepDefinitionsUI.java
+++ springPressOrder/src/test/java/be/springPressOrder/StepDefinitionsUI.java
@@ -1,87 +1,76 @@
 package be.springPressOrder;
 
-import cucumber.api.PendingException;
+import be.springPressOrder.PageObjects.MainPage;
+import be.springPressOrder.PageObjects.PressOrderPage;
+import be.springPressOrder.PageObjects.SignInPage;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.boot.devtools.env.DevToolsHomePropertiesPostProcessor;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class StepDefinitionsUI {
 
-
-    WebDriver driver;
+    WebDriver driver = new ChromeDriver();
+    MainPage mainPage = new MainPage(driver);
+    SignInPage signInPage = new SignInPage(driver);
+    PressOrderPage pressOrderPage = new PressOrderPage(driver);
 
 
     @Given("^particulier is ingelogd$")
     public void particulierIsIngelogd() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.navigate().to("http://localhost:8080/login");
-
-        driver.findElement(By.id("username")).sendKeys("admin");
-        driver.findElement(By.id("password")).sendKeys("password");
-
-        driver.findElement(By.id("btnSubmit")).click();
+        mainPage.navigateToSignInPage();
+        signInPage.signIn("username", "password");
     }
 
 
     @And("^de particulier begeeft zich op de pagina om een nieuwe pressorder aan te maken$")
     public void deParticulierBegeeftZichOpDePaginaOmEenNieuwePressorderAanTeMaken() {
-        driver.navigate().to("http://localhost:8080/pressorder/new");
+        pressOrderPage = mainPage.navigateToPressOrderPage();
     }
 
-    @When("^particulier (\\d+) peren invoert in het veld \"([^\"]*)\"$")
-    public void particulierPerenInvoertInHetVeld(int aantal, String id) throws Throwable {
-        driver.findElement(By.id(id)).sendKeys(String.valueOf(aantal));
+    @When("^particulier (\\d+) peren invoert in het veld$")
+    public void particulierPerenInvoertInHetVeld(int aantal) throws Throwable {
+        pressOrderPage.voerPerenIn(aantal);
     }
 
-    @And("^hij duidt peer aan in de combobox \"([^\"]*)\"$")
-    public void hijDuidtPeerAanInDeCombobox(String id) throws Throwable {
-        Select select = new Select(driver.findElement(By.id(id)));
-        select.selectByValue("2");
-    }
 
     @And("^hij klikt op Submit$")
     public void hijKliktOpSubmit() {
-        driver.findElement(By.id("btnSubmit")).click();
+        pressOrderPage.bevestigPersOrder();
     }
 
 
-    @When("^de particulier (\\d+) appelen invoert in het veld \"([^\"]*)\"$")
-    public void deParticulierAppelenInvoertInHetVeld(int aantal, String id) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        driver.findElement(By.id(id)).sendKeys(String.valueOf(aantal));
+    @When("^de particulier (\\d+) appelen invoert in het veld$")
+    public void deParticulierAppelenInvoertInHetVeld(int aantal) throws Throwable {
+        pressOrderPage.voerAppelenIn(aantal);
     }
 
-    @And("^hij duidt appel aan in de combobox \"([^\"]*)\"$")
-    public void hijDuidtAppelAanInDeCombobox(String id) throws Throwable {
-        Select select = new Select(driver.findElement(By.id(id)));
-        select.selectByValue("1");
-    }
-
-    @Then("^krijgt de particulier de volgende boodschap te zien : \"([^\"]*)\" in het field \"([^\"]*)\"$")
-    public void krijgtDeParticulierDeVolgendeBoodschapTeZienInHetField(String msg, String field) throws Throwable {
-        assertThat(driver.findElement(By.id(field)).getText()).isEqualTo(msg);
+    @Then("^krijgt de particulier de volgende boodschap te zien : \"([^\"]*)\" in het veld$")
+    public void krijgtDeParticulierDeVolgendeBoodschapTeZienInHetField(String msg) throws Throwable {
+        String text = pressOrderPage.bekijkFoutMelding();
+        assertThat(text).isEqualTo(msg);
     }
 
 
     @Then("^krijgt de particulier (\\d+) flessen$")
     public void krijgtDeParticulierFlessen(int aantal) {
-        WebElement element = (driver.findElement(By.id("resultaat")));
-        String text = element.getText();
+        String text = pressOrderPage.bekijkResultaat();
         assertThat(text).contains(String.valueOf(aantal));
     }
 
+    @And("^sluit de pagina$")
+    public void sluitDePagina() {
+        mainPage.closeDriver();
+    }
 }
 
--- springPressOrder/src/test/resources/be.springPressOrder/voegPersOrderToeUI.feature
+++ springPressOrder/src/test/resources/be.springPressOrder/voegPersOrderToeUI.feature
@@ -8,27 +8,32 @@ Feature: Fruit meegeven via UI
   Scenario: Particulier geeft te weinig peren mee via UI
     Given particulier is ingelogd
     And de particulier begeeft zich op de pagina om een nieuwe pressorder aan te maken
-    When particulier 3 peren invoert in het veld "fruitAmount"
-    And hij duidt peer aan in de combobox "fruitId"
+    When particulier 3 peren invoert in het veld
     And hij klikt op Submit
-    Then krijgt de particulier de volgende boodschap te zien : "Het minimum aantal peren is 4" in het field "msgError"
-
+    Then krijgt de particulier de volgende boodschap te zien : "Het minimum aantal peren is 4" in het veld
+    And sluit de pagina
 
   Scenario: Particulier geeft te weinig appelen mee via UI
     Given particulier is ingelogd
     And de particulier begeeft zich op de pagina om een nieuwe pressorder aan te maken
-    When de particulier 2 appelen invoert in het veld "fruitAmount"
-    And hij duidt appel aan in de combobox "fruitId"
+    When de particulier 2 appelen invoert in het veld
     And hij klikt op Submit
-    Then krijgt de particulier de volgende boodschap te zien : "Het minimum aantal appelen is 3" in het field "msgError"
+    Then krijgt de particulier de volgende boodschap te zien : "Het minimum aantal appelen is 3" in het veld
+    And sluit de pagina
 
   Scenario: De particulier maakt een pers-opdracht van een correct aantal appelen, zonder bonus
-    Given particulier heeft 50 appelen en wilt een pers-opdracht doen
-    When de particulier deze appelen afgeeft
+    Given particulier is ingelogd
+    And de particulier begeeft zich op de pagina om een nieuwe pressorder aan te maken
+    When de particulier 50 appelen invoert in het veld
+    And hij klikt op Submit
     Then krijgt de particulier 17 flessen
-
+    And sluit de pagina
 
   Scenario: De particulier maakt een pers-opdracht van een correct aantal peren, zonder bonus
-    Given particulier heeft 50 peren en wilt een pers-opdracht doen
-    When de particulier deze peren afgeeft
+    Given particulier is ingelogd
+    And de particulier begeeft zich op de pagina om een nieuwe pressorder aan te maken
+    When particulier 50 peren invoert in het veld
+    And hij klikt op Submit
     Then krijgt de particulier 12 flessen
+    And sluit de pagina
+
